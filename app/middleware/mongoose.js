3f441d0d8f4cf3c489d8277f8f318a887ab605de
kibs86 <kibs086@gmail.com>
2016-12-15 13:18:31 -0500

    Initial setup of back-end repo
    - Used express-api-template as a starting place

---
+++ app/middleware/mongoose.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const uri = process.env.MONGODB_URI || 'mongodb://localhost/TimeCapsule-Backend';
+mongoose.Promise = global.Promise;
+mongoose.connect(uri);
+
+module.exports = mongoose;
