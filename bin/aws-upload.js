b3caa851d248957bb2a33941eb6d35499472c37a
“coffeeisamazing” <caleb.searles@libertymutual.com>
2016-12-16 09:50:58 -0500

    Add updated aws-upload starter code.
    Add updated aws-upload starter code with fixed command prompt testing.

--- bin/aws-upload.js
+++ bin/aws-upload.js
@@ -2,31 +2,54 @@
 
 require('dotenv').load();
 
-const s3Upload = require('../lib/aws-s3-upload.js');
+const s3Upload = require('../lib/aws-s3-upload');
 const mongoose = require('../app/middleware/mongoose');
 const Upload = require('../app/models/upload');
 
-// const mime = require('mime');
+const mime = require('mime');
 
+// Old `file` object =>
+// let file = {
+//   path: process.argv[2],
+//   title: process.argv[3]
+// };
+
+// req.file looks like this:
+// { fieldname: 'image[file]',
+//   originalname: 'payne.jpg',
+//   encoding: '7bit',
+//   mimetype: 'image/jpeg',
+//   destination: '/tmp/',
+//   filename: '5e215bd26b83220a4c75f5f9d1f7c5cd',
+//   path: '/tmp/5e215bd26b83220a4c75f5f9d1f7c5cd',
+//   size: 26116 }
+
+// new `file` object
 let file = {
   path: process.argv[2],
-  title: process.argv[3]
+  originalname: process.argv[2],
+  title: process.argv[3],
 };
 
-// let contentType = mime.lookup(file.path);
+let contentType = mime.lookup(file.originalname);
+
+file.mimetype = contentType;
+
+console.log("file looks like", file);
 
 s3Upload(file)
   .then(function(response){
     console.log("inside then block");
-    console.log("data is ", response);
+    console.log("response is ", response);
     console.log("url is ", response.Location);
-    // Upload.create returns a promise so we can just return it here
     return Upload.create({
       url: response.Location,
-      // don't need a random title when we upload to mongo, only when we upload
-      // to AWS so we can use file.title here
-      title: file.title
+      title: file.title,
     });
+  })
+  .then(function(upload){
+    console.log("mongo create was successful");
+    console.log("upload is ", upload);
   })
   .catch(console.error)
   .then(function(){
